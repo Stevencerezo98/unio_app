2f2ab095e7cbe71b1bdec2eab24c204a45ff61ca
stevencerezo42 <stevencerezo42@gmail.com>
2026-01-12 05:47:28 +0000

    Migración a nuevo repositorio unio_app

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:zoom_clone_tutorial/main.dart';
+import 'package:unio/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
